d1e666a4d7fbdc051f32a3873beae5faa9462713
Guillermo Brachetta <brachetta@me.com>
2021-02-03 15:34:20 +0100

    Fetch data from api

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match: { params } }) => {
-  const product = products.find((p) => p._id === params.id);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [params]);
+
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
