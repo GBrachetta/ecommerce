18c6c7f3ae4986afb2b07fa58c8b55e0a46dbfe8
Guillermo Brachetta <brachetta@me.com>
2021-02-10 18:03:14 +0100

    Add UsersEditScreen component and functionality

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UsersEditScreen';
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
           <Route path="/order/:id" component={OrderScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
-          <Route path="/admin/userlist/" component={UserListScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
         </Container>
       </main>
       <Footer />
